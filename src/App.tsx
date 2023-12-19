68a816f0ffd822d8555afcec8aeaeb094d5582e9
Georgefemiwise <georgewise1010@gmail.com>
2023-12-19 22:36:37 +0000

    installed: tailwid and daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="card">
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col gap-4 w-52">
+        <div className="flex gap-4 items-center">
+          <div className="skeleton w-16 h-16 rounded-full shrink-0"></div>
+          <div className="flex flex-col gap-4">
+            <div className="skeleton h-4 w-20"></div>
+            <div className="skeleton h-4 w-28"></div>
+          </div>
+        </div>
+        <div className="skeleton h-32 w-full"></div>
 
-        <h1 className="">Maildrop client</h1>
-        <button className="btn" onClick={() => setCount((count) => count + 1)}>
-          {count}
-        </button>
+        <div className="skeleton min-h-4 max-w-28"></div>
+        <div className="skeleton min-h-4 min-w-full"></div>
+        <div className="skeleton min-h-4 min-w-full"></div>
+        <p className="capitalize font-thin text-xs text-primary">under development</p>
+        
       </div>
-    </>
+    </div>
   );
 }
 
