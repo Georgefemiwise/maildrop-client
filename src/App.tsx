1df694bc216dd368d172847f145c1782c7d0d683
Georgefemiwise <georgewise1010@gmail.com>
2023-12-20 09:41:39 +0000

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
