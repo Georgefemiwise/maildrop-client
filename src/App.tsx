54ddef8ff2d65ad6c688d73b9b30cfc85c9ac741
Georgefemiwise <georgewise1010@gmail.com>
2023-12-28 11:42:34 +0000

    added: bar chart from recharts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
+import { useEffect, useState } from "react";
+import StudentChart from "./components/studendData";
+
 function App() {
+  const [data, setdata] = useState();
+
+  //  API endpoint
+  const getdata = () => {
+    const apiUrl = "http://127.0.0.1:8000/api/students/";
+
+    // Make a GET request using fetch
+    fetch(apiUrl)
+      .then((response) => {
+        // Check if the response is successful (status code in the range 200-299)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        // Parse the response as JSON
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the data
+        setdata(data);
+        console.log("Data received:", data);
+        //perform further actions with the data here
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <div className="flex flex-col gap-4 w-52">
-        <div className="flex gap-4 items-center">
-          <div className="skeleton w-16 h-16 rounded-full shrink-0"></div>
-          <div className="flex flex-col gap-4">
-            <div className="skeleton h-4 w-20"></div>
-            <div className="skeleton h-4 w-28"></div>
+    <>
+      <div className="flex lg:flex-row flex-col gap-3 p-3 justify-center items-center h-screen">
+        <div className="card w-full bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Grand total student Programs</h2>
+            <StudentChart studentData={data} />
           </div>
         </div>
-        <div className="skeleton h-32 w-full"></div>
-
-        <div className="skeleton min-h-4 max-w-28"></div>
-        <div className="skeleton min-h-4 min-w-full"></div>
-        <div className="skeleton min-h-4 min-w-full"></div>
-        <p className="capitalize font-thin text-xs text-primary">
-          under development
-        </p>
+        <div className="card skeleton w-full h-[500px] bg-neutral text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title skeleton"></h2>
+            <p></p>
+          </div>
+        </div>
+        <div className="card w-full bg-neutral h-[500px] skeleton text-neutral-content">
+          <div className="card-body items-center text-center">
+            <h2 className="card-title"></h2>
+            <p></p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/studendData.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+interface Student {
+  id: any;
+  index: string;
+  academic_year: string;
+  program: string;
+  email: string;
+  course: string;
+  graduation_year: string;
+}
+
+interface ChartData {
+  program: string;
+  count: number;
+}
+
+interface StudentChartProps {
+  studentData?: Student[];
+}
+
+const StudentChart: React.FC<StudentChartProps> = ({ studentData=[] }) => {
+  // Ensure studentData is defined before proceeding
+  if (!studentData) {
+    return null; // Or display a loading indicator or an appropriate message
+  }
+
+  // Extract relevant data for plotting
+  const programCounts: Record<string, number> = studentData.reduce(
+    (accumulator, student) => {
+      accumulator[student.program] = (accumulator[student.program] || 0) + 1;
+      return accumulator;
+    },
+    {} as Record<string, number>
+  );
+
+  // Convert data to an array of objects
+  const chartData: ChartData[] = Object.keys(programCounts).map((program) => ({
+    program,
+    count: programCounts[program],
+  }));
+
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart
+        data={chartData}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="program" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="count" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default StudentChart;
