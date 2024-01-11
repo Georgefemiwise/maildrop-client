c7898007e99243ef3f6687707f2470385f54a06e
Georgefemiwise <georgewise1010@gmail.com>
2024-01-11 13:40:35 +0000

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import fetchtData from "./components/utils";
 
 function App() {
   const { data } = fetchtData();
+  console.log(data);
 
   useEffect(() => {}, []);
 
@@ -19,7 +20,7 @@ function App() {
         </div>
         <div className="card skeleton w-full h-[500px] bg-neutral text-neutral-content">
           <div className="card-body items-center text-center">
-            <h2 className="card-title skeleton"></h2>
+            <h2 className="card-title"></h2>
             <StudentLineChart studentData={data} />
           </div>
         </div>
